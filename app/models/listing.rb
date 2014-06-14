4236ff5b5b9b2f8595a95850f0b271c6fc13274e
kaushik <kkt2123@columbia.edu>
2014-06-14 00:54:14 -0700

    localhost for dev and heroku for prod

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,16 @@
 class Listing < ActiveRecord::Base
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	else
 	  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
 	  					:storage => :dropbox,
-      					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+      					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+      					:path => ":style/:id_:filename"
 	  
 	  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+	end
 
 end
