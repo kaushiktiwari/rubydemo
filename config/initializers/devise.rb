7fd0fe5029dc3bcf26a79bc990b1bf5ba4b6ab42
kaushik <kkt2123@columbia.edu>
2014-06-18 17:14:06 -0700

    used get for delete

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -223,10 +223,9 @@ Devise.setup do |config|
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html]
 
-  # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
-  config.sign_out_via = :get if Rails.env.test?
-
+  # The default HTTP method use to sign out a resource. Default is :delete.
+  config.sign_out_via = :get
+  
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
