91cd68dbd3a8c06ca1fbc14508ddf06af3f98ed3
kaushik <kkt2123@columbia.edu>
2014-06-18 17:33:10 -0700

    made changes in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+ # devise_for :users
   resources :listings
+devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+end
 
   get 'pages/about'
 
